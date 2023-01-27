3e375cc07035bb89b2ebe17a621f321116581882
ujjawal <noobtuber20103152>
2023-01-27 21:56:43 +0530

    vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
+import { auth, db } from '@/firebase-config';
 import '@/styles/globals.css'
 import { Analytics } from '@vercel/analytics/react';
-
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+// useEffect
 export default function App({ Component, pageProps }) {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+  useEffect(() => {
+    if(!router){
+      router.push("/")
+    }
+  }, [user])
+  
   return <>
-   <Component {...pageProps} />
-   <Analytics/>
+    <Component {...pageProps} />
+    <Analytics />
   </>
 }
